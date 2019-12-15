01628b71727f00907ffcd8e0160113eff554de21
Kyzyurova Evgenia <kyzyurova.evgenia@gmail.com>
2019-12-15 15:27:48 +0200

    commit2

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ const Layout = props => {
     <div className="Layout">
       <Head>
         <title>SagasLife</title>
+        <link ref="shortcut icon" href="../public/logo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
       </Head>
