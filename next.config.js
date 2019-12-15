8188a33926c0314da964f5b8b4aa75fc443c8dfb
Bohdan Lysov <BogUkraine@gmail.com>
2019-12-15 16:37:31 +0200

    add images and head

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-const withCSS = require('@zeit/next-css')
+const withCSS = require('@zeit/next-css');
+
 module.exports = withCSS({
+    webpack: (config, { dev }) => {
+        config.devtool = 'source-map'
   
-})
+        for (const plugin of config.plugins) {
+          if (plugin['constructor']['name'] === 'UglifyJsPlugin') {
+            plugin.options.sourceMap = true
+            break
+          }
+        }
+  
+        return config
+      }
+});
--- pages/_document.js
+++ pages/_document.js
@@ -15,9 +15,7 @@ class DocumentComponent extends Document {
     return (
       <html>
         <Head>
-            <title>SagasLife</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
+            {this.props.styleTags}
         </Head>
 
         <body>
