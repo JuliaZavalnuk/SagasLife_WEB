a90755ce5fa384158752ee2f8fb64a265c1ace9b
Bohdan Lysov <BogUkraine@gmail.com>
2019-12-15 03:06:30 +0200

    simple routing

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Layout = props => {
+  return (
+    <div className="Layout">
+      <Head>
+        <title>SagasLife</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+      </Head>
+      <div className="Content">{props.children}</div>
+    </div>
+  );
+};
+
+export default Layout;
