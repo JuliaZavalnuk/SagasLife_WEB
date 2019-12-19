8107a58fbb47e24159d49d9a41d03bfed65f9606
Bohdan Lysov <BogUkraine@gmail.com>
2019-12-19 23:16:36 +0200

    loading for images

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,11 @@
 import Meta from "./Meta";
 import ReactNotifications from 'react-notifications-component';
 
-// Notifications styles
 import '../styles/notifications/notificationsPopUp.css';
 import 'animate.css';
 
-//Progress styles 
 import '../styles/progressBar/progress.css';
 
-//main style
 import '../styles/generic/main.css';
 
 const Layout = props => {
